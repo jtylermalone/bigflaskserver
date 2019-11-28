a1a1d2cbba3b0d1b5c5ec23b9905391c7364df01
Tyler <malonet@g.cofc.edu>
2019-11-28 16:45:14 -0500

    truly have all voting functionaly working, just need to put in container

--- app/vote/voting.py
+++ app/vote/voting.py
@@ -2,28 +2,49 @@ from flask import render_template, session
 from app import redis_cli
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, StringField, RadioField
-from wtforms.validators import DataRequired, NumberRange
+from wtforms.validators import DataRequired
 from . import vote
+
 
 class VoteForm(FlaskForm):
     userName = StringField("Enter your name", validators=[DataRequired()])
     userVote = RadioField("Here are your voting options", choices=[('cats', 'cats'),
                                                                    ('dogs', 'dogs'),
-                                                                   ('fish', 'fish')])
+                                                                   ('fish', 'fish'),
+                                                                   ('birds', 'birds'),
+                                                                   ('turtles', 'turtles'),
+                                                                   ('goats', 'goats')
+                                                                   ])
     submit = SubmitField("Cast your vote!")
+
 
 @vote.route("/voting", methods=['POST', 'GET'])
 def vote():
-    message = "Please register your vote below!"
+
+    # default message when user first goes to voting page
+    message = "Please vote below! You may only cast ONE (1) vote!"
+
+    # instantiates VoteForm
     voteForm = VoteForm()
+
+    # must initialize vote count for each choice to 0
     dogVotes = 0
     catVotes = 0
     fishVotes = 0
+    birdVotes = 0
+    turtleVotes = 0
+    goatVotes = 0
+    session["alreadyVoted"] = "no"
     if voteForm.validate_on_submit():
-        userName = voteForm.userName.data
-        alreadyVoted = redis_cli.exists(f"{userName}")
+        session["alreadyVoted"] = "yes"
         userChoice = voteForm.userVote.data
-        session['userChoice'] = userChoice
+        userName = voteForm.userName.data
+        alreadyInDatabase = redis_cli.exists(f"{userName}")
+        if not alreadyInDatabase:
+            redis_cli.set(f"{userName}", userChoice)
+            message = "Thank you for voting! Here are the results so far:"
+        else:
+            message = "Hey!! You've already voted! You aren't allowed to vote again!"
         listOfKeys = redis_cli.keys("*")
         for key in listOfKeys:
             userVote = redis_cli.get(key).decode("utf-8")
@@ -33,16 +54,14 @@ def vote():
                 catVotes = catVotes + 1
             if userVote == "fish":
                 fishVotes = fishVotes + 1
-        if not alreadyVoted:
-            redis_cli.set(f"{userName}", userChoice)
-            message = "Thank you for voting! Here are the results so far:"
-        else:
-            message = "Hey!! You've already voted! You aren't allowed to vote again!"
-
-        return render_template("voting.html", message=message, voteForm=voteForm,
-                               dogVotes=dogVotes, catVotes=catVotes, fishVotes=fishVotes, alreadyVoted=alreadyVoted)
-
-
-
+            if userVote == "birds":
+                birdVotes = birdVotes + 1
+            if userVote == "turtles":
+                turtleVotes = turtleVotes + 1
+            if userVote == "goats":
+                goatVotes = goatVotes + 1
+        return render_template("voting.html", message=message, voteForm=voteForm, dogVotes=dogVotes, catVotes=catVotes,
+                               fishVotes=fishVotes, birdVotes=birdVotes, turtleVotes=turtleVotes, goatVotes=goatVotes,
+                               alreadyInDatabase=alreadyInDatabase)
     return render_template("voting.html", message=message, voteForm=voteForm)
 
