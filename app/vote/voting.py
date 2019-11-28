1d6abdcff9c2efcd3aed6ad4583f62dc60a6c83f
Tyler <malonet@g.cofc.edu>
2019-11-28 15:48:48 -0500

    got voting display working properly now (i think)

--- app/vote/voting.py
+++ app/vote/voting.py
@@ -6,38 +6,42 @@ from wtforms.validators import DataRequired, NumberRange
 from . import vote
 
 class VoteForm(FlaskForm):
-    userName = StringField("Enter your name")
+    userName = StringField("Enter your name", validators=[DataRequired()])
     userVote = RadioField("Here are your voting options", choices=[('cats', 'cats'),
-                                      ('dogs', 'dogs')])
+                                                                   ('dogs', 'dogs'),
+                                                                   ('fish', 'fish')])
     submit = SubmitField("Cast your vote!")
 
 @vote.route("/voting", methods=['POST', 'GET'])
 def vote():
     message = "Please register your vote below!"
     voteForm = VoteForm()
+    dogVotes = 0
+    catVotes = 0
+    fishVotes = 0
     if voteForm.validate_on_submit():
         userName = voteForm.userName.data
+        alreadyVoted = redis_cli.exists(f"{userName}")
         userChoice = voteForm.userVote.data
         session['userChoice'] = userChoice
+        listOfKeys = redis_cli.keys("*")
+        for key in listOfKeys:
+            userVote = redis_cli.get(key).decode("utf-8")
+            if userVote == "dogs":
+                dogVotes = dogVotes + 1
+            if userVote == "cats":
+                catVotes = catVotes + 1
+            if userVote == "fish":
+                fishVotes = fishVotes + 1
+        if not alreadyVoted:
+            redis_cli.set(f"{userName}", userChoice)
+            message = "Thank you for voting! Here are the results so far:"
+        else:
+            message = "Hey!! You've already voted! You aren't allowed to vote again!"
 
-        # this returns 0 if it doesn't exist and 1 if it does... in python 0 is equivalent
-        # to false and 1 is equivalent to true
-        alreadyVoted = redis_cli.exists(f"{userName}")
-        if alreadyVoted:
-            dogVotes = 0
-            catVotes = 0
-            message = "You've already voted! You aren't allowed to vote again!"
-            listOfKeys = redis_cli.keys("*")
-            for key in listOfKeys:
-                userVote = redis_cli.get(key).decode("utf-8")
-                if userVote == "dogs":
-                    dogVotes = dogVotes + 1
-                if userVote == "cats":
-                    catVotes = catVotes + 1
+        return render_template("voting.html", message=message, voteForm=voteForm,
+                               dogVotes=dogVotes, catVotes=catVotes, fishVotes=fishVotes, alreadyVoted=alreadyVoted)
 
-            return render_template("voting.html", message=message, voteForm=voteForm, listOfKeys=listOfKeys,
-                                   dogVotes=dogVotes, catVotes=catVotes, alreadyVoted=alreadyVoted, userVote=userVote)
-        redis_cli.set(f"{userName}", userChoice)
 
 
     return render_template("voting.html", message=message, voteForm=voteForm)
